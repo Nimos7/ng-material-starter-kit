c70596b6ef927ae03cfe62840e9fe63f30974322
Nimos7 <jakub.jachimekk@gmail.com>
2022-10-19 12:34:17 +0200

    feat: products list added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductListComponentModule } from './components/product-list/product-list.component-module';
+import { ProductServiceModule } from './services/product.service-module';
+import { HomePageComponentModule } from './components/home-page/home-page.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'producs', component: ProductListComponent },
+    { path: '', component: HomePageComponent }]),
+    ProductListComponentModule, ProductServiceModule, HomePageComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {ProductListComponentModule} from "./components/product-list/product-list.component-module";
+import {HomePageComponentModule} from "./components/home-page/home-page.component-module";
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,12 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ProductListComponentModule,
+    HomePageComponentModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
