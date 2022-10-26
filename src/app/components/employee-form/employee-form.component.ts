9d7312dde6155f67453681b4b77bcf3e2ddd793b
Nimos7 <jakub.jachimekk@gmail.com>
2022-10-26 17:40:07 +0200

    feat: Forms exercise 5- Create register form+ fixed details

--- src/app/components/employee-form/employee-form.component.ts
+++ src/app/components/employee-form/employee-form.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { EmployeeService } from '../../services/employee.service';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {EmployeeService} from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-form',
@@ -12,18 +12,19 @@ import { EmployeeService } from '../../services/employee.service';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl(),
-    age: new FormControl(),
-    salary: new FormControl()
+    salary: new FormControl(),
+    age: new FormControl()
   });
 
   constructor(private _employeeService: EmployeeService) {
+
   }
 
   onEmployeeFormSubmitted(employeeForm: FormGroup): void {
     this._employeeService.create({
-      name:employeeForm.get('name')?.value,
-      salary:employeeForm.get('salary')?.value,
-      age:employeeForm.get('age')?.value
+      name: employeeForm.get('name')?.value,
+      age: employeeForm.get('age')?.value,
+      salary: employeeForm.get('salary')?.value
     }).subscribe();
   }
 }
